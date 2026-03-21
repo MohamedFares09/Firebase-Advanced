85d2691009b1064a54dc009f0f11ebe9ad56dcf0
Mohamed Fares <mofares010@gmail.com>
2026-03-22 00:37:39 +0200

    add home page

---
+++ lib/home/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_advanced/auth/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginPage()),
+              );
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+        title: const Text('Home Page'),
+      ),
+      body: const Center(child: Text('Home Page')),
+    );
+  }
+}
