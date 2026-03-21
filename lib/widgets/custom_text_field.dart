9d75a357b0d54971cafa477af6d35d2a114058fa
Mohamed Fares <mofares010@gmail.com>
2026-03-21 22:23:41 +0200

    add text form field

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.white),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.deepPurple),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        hintText: hintText,
+        fillColor: Colors.grey[200],
+        filled: true,
+      ),
+    );
+  }
+}
