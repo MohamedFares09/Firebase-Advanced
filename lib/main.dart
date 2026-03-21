0430325a87303a5915bcb12615139c869752bfb1
Mohamed Fares <mofares010@gmail.com>
2026-03-22 00:36:16 +0200

    if user login navigator to home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,43 @@
 import 'package:firebase_advanced/auth/login_page.dart';
 import 'package:firebase_advanced/firebase_options.dart';
+import 'package:firebase_advanced/home/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main()async {
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:LoginPage(),
+      home: FirebaseAuth.instance.currentUser == null
+          ? LoginPage()
+          : HomePage(),
     );
   }
 }
