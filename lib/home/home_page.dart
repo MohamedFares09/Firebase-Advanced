9de385a5c2a71cdecfc67feee634203cbfe190ae
Mohamed Fares <mofares010@gmail.com>
2026-03-25 01:53:52 +0200

    edit in home page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,11 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_advanced/auth/login_page.dart';
 import 'package:firebase_advanced/category/add_category.dart';
+import 'package:firebase_advanced/category/update_category.dart';
 import 'package:firebase_advanced/l10n/app_localizations.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isLoading = true;
+  List data = [];
+  getData() async {
+    if (FirebaseAuth.instance.currentUser != null) {
+      QuerySnapshot snapshot = await FirebaseFirestore.instance
+          .collection('category')
+          .where('id', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .get();
+      data.addAll(snapshot.docs);
+      Future.delayed(Duration(seconds: 2));
+      isLoading = false;
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,29 +64,81 @@ class HomePage extends StatelessWidget {
         ],
         title: Text(l10n.homePageTitle),
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisExtent: 160,
-        ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : GridView.builder(
+              itemCount: data.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisExtent: 160,
+              ),
 
-        itemBuilder: (context, index) {
-          return Card(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Image.asset('assets/folder.png', height: 100),
-                  Text(l10n.homePageTitle),
-                ],
-              ),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onLongPress: () async {
+                    await showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text('Wrong'),
+                          content: Text(
+                            'Are you sure you want to delete this category?',
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => UpdateCategory(
+                                      id: data[index].id,
+                                      oldName: data[index]['name'],
+                                    ),
+                                  ),
+                                );
+                              },
+                              child: Text('Update'),
+                            ),
+                            TextButton(
+                              onPressed: () async {
+                                await FirebaseFirestore.instance
+                                    .collection('category')
+                                    .doc(data[index].id)
+                                    .delete();
+
+                                Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(
+                                    builder: (context) => HomePage(),
+                                  ),
+                                );
+                                Fluttertoast.showToast(
+                                  msg: 'Category deleted successfully',
+                                );
+                              },
+                              child: Text('Delete'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Card(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Image.asset('assets/folder.png', height: 100),
+                          Text(data[index]['name']),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
